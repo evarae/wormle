8b463516d49c0096d20adccf59ec7512396a8c75
evarae <evarae.mclean@gmail.com>
2024-07-02 22:01:47 +1200

    Added responsive path behaviour

--- src/app/game/grid/Grid.tsx
+++ src/app/game/grid/Grid.tsx
@@ -1,125 +1,169 @@
-import React, { useRef, useState, useEffect, useMemo } from 'react';
-import { TileType, Coordinates, GameState, Tile } from '../../../types/types';
-import { getTileKey, getTileTypeForPathIndex, getValidMovesBetweenPoints } from '../GameEngine';
-import GridTile, { GridTileProps } from '../tiles/GridTile';
-import InvisibleTile from '../tiles/InvisibleTile';
+import React, { useRef, useState, useEffect, useMemo } from "react";
+import { TileType, Coordinates, GameState, Tile } from "../../../types/types";
+import {
+  getTileKey,
+  getTileTypeForPathIndex,
+  getValidMovesBetweenPoints,
+} from "../GameEngine";
+import GridTile, { GridTileProps } from "../tiles/GridTile";
+import InvisibleTile from "../tiles/InvisibleTile";
 
-export default function Grid(props:GridProps){
+export default function Grid(props: GridProps) {
   const textInputRefs = useRef<Record<string, HTMLButtonElement>>({});
   const [hoveredCoordinates, setHoveredCoordinates] = useState<Coordinates>();
-  
+
   function refocusPath() {
     if (props.gameState.path.length > 0) {
       const ref =
-          textInputRefs.current[
-            getTileKey(props.gameState.path[props.gameState.path.length - 1])
-          ];
+        textInputRefs.current[
+          getTileKey(props.gameState.path[props.gameState.path.length - 1])
+        ];
       if (ref !== undefined) {
         ref.focus();
       }
     }
   }
 
-  function onClickCallback(tile:Tile){
+  function onClickCallback(tile: Tile) {
     setHoveredCoordinates(undefined);
-    if(!props.isReadOnly){
+    if (!props.isReadOnly) {
       props.tileOnClickCallback(tile);
     }
   }
-  
-  function onMouseEnter(tile:Tile){
+
+  function onMouseEnter(tile: Tile) {
     setHoveredCoordinates(tile.coordinates);
   }
 
-  function onMouseLeave(){
+  function onMouseLeave() {
     setHoveredCoordinates(undefined);
   }
 
   //Refocuses when the path changes
   useEffect(() => {
-    if(!props.isReadOnly){
+    if (!props.isReadOnly) {
       refocusPath();
     }
   }, [props.gameState]);
-      
+
   const renderedTiles = useMemo(() => {
     const displayModifiers: Record<string, TileDisplayMod> = {};
 
     //Set tile types
-    if(props.gameState.path !== undefined){
+    if (props.gameState.path !== undefined) {
       props.gameState.path.forEach((c, index) => {
-        displayModifiers[getTileKey(c)] = {tileType: getTileTypeForPathIndex(index, props.gameState.path)};
+        displayModifiers[getTileKey(c)] = {
+          tileType: getTileTypeForPathIndex(index, props.gameState.path),
+        };
       });
     }
 
     //Set preview tile stuff
-    if(hoveredCoordinates !== undefined && props.gameState.path !== undefined){
-      const lastPathCoordinate = props.gameState.path[props.gameState.path.length-1];
-      const previewPath = getValidMovesBetweenPoints(props.gameState, lastPathCoordinate, hoveredCoordinates);
+    if (
+      hoveredCoordinates !== undefined &&
+      props.gameState.path !== undefined
+    ) {
+      const lastPathCoordinate =
+        props.gameState.path[props.gameState.path.length - 1];
+      const previewPath = getValidMovesBetweenPoints(
+        props.gameState,
+        lastPathCoordinate,
+        hoveredCoordinates
+      );
 
-      if(previewPath.length > 0){
+      if (previewPath.length > 0) {
         previewPath.unshift(lastPathCoordinate);
       }
       previewPath.forEach((c, index) => {
-        displayModifiers[getTileKey(c)] = {...displayModifiers[getTileKey(c)], previewTileType: getTileTypeForPathIndex(index, previewPath), previewString: props.gameState.pathLetters[props.gameState.path.length -1 + index]};
+        displayModifiers[getTileKey(c)] = {
+          ...displayModifiers[getTileKey(c)],
+          previewTileType: getTileTypeForPathIndex(index, previewPath),
+          previewString:
+            props.gameState.pathLetters[
+              props.gameState.path.length - 1 + index
+            ],
+        };
       });
     }
-    
+
     const wordElements = [];
-  
+
     for (let j = 0; j < props.gameState.gridSize.y; j++) {
       const tileElements = [];
-    
+
       for (let i = 0; i < props.gameState.gridSize.x; i++) {
         const t = props.gameState.tiles[getTileKey({ x: i, y: j })];
         let mods = displayModifiers[getTileKey({ x: i, y: j })];
 
-        if(mods == undefined){
-          mods = {tileType: TileType.Empty};
+        if (mods == undefined) {
+          mods = { tileType: TileType.Empty };
         }
 
         if (t !== undefined) {
-          const tileProps:GridTileProps = props.isReadOnly? 
-            {isReadOnly: true, tile: t, ...mods} :
-            {isReadOnly: false, tile: t, onClickCallback, onMouseEnter, onMouseLeave, ...mods};
-  
+          const tileProps: GridTileProps = props.isReadOnly
+            ? { isReadOnly: true, tile: t, ...mods }
+            : {
+                isReadOnly: false,
+                tile: t,
+                onClickCallback,
+                onMouseEnter,
+                onMouseLeave,
+                ...mods,
+              };
+
           tileElements.push(
-            <GridTile {...tileProps} ref={(ref) => textInputRefs.current[getTileKey(t.coordinates)] = ref!}/>
+            <GridTile
+              key={`grid-tile-${getTileKey(t.coordinates)}`}
+              {...tileProps}
+              ref={(ref) =>
+                (textInputRefs.current[getTileKey(t.coordinates)] = ref!)
+              }
+            />
           );
         } else {
           tileElements.push(<InvisibleTile key={getTileKey({ x: i, y: j })} />);
         }
       }
-    
+
       wordElements.push(
-        <div key={j} className={`word-container tile-color-${(j%3)+1}`}>
+        <div
+          key={`grid-word-${j}`}
+          className={`word-container tile-color-${(j % 3) + 1}`}
+        >
           {tileElements}
         </div>
       );
     }
-  
+
     return wordElements;
   }, [props.gameState, hoveredCoordinates]);
-  
-  return (<div className={`grid-container grid-container-${props.gridSize?? 'large'}`}>{renderedTiles}</div>);
+
+  return (
+    <div
+      className={`grid-container grid-container-${props.gridSize ?? "large"}`}
+    >
+      {renderedTiles}
+    </div>
+  );
 }
 
-  type TileDisplayMod = {
-    tileType:TileType,
-    previewString?:string,
-    previewTileType?:TileType
-  }
+type TileDisplayMod = {
+  tileType: TileType;
+  previewString?: string;
+  previewTileType?: TileType;
+};
 
-  type GridProps = {
-    gridSize?:GridSize
-    gameState: GameState,
-    isReadOnly: true
-  } | {
-    gridSize?:GridSize
-    gameState: GameState,
-    isReadOnly: false,
-    tileOnClickCallback: (tile: Tile) => void
-  }
+type GridProps =
+  | {
+      gridSize?: GridSize;
+      gameState: GameState;
+      isReadOnly: true;
+    }
+  | {
+      gridSize?: GridSize;
+      gameState: GameState;
+      isReadOnly: false;
+      tileOnClickCallback: (tile: Tile) => void;
+    };
 
-  type GridSize = 'large' | 'medium' | 'small' | 'xsmall';
-  
+type GridSize = "large" | "medium" | "small" | "xsmall";
--- src/app/game/grid/Path.tsx
+++ src/app/game/grid/Path.tsx
@@ -2,45 +2,104 @@ import React from "react";
 import { GameState, Tile, TileType } from "../../../types/types";
 import GridTile from "../tiles/GridTile";
 import "./Path.css";
+import InvisibleTile from "../tiles/InvisibleTile";
 
 export default function Path(props: Props) {
-  const mappedTiles = props.gameState.pathLetters.map((t, index) => {
-    const tile: Tile = {
-      guess: t,
-      value: t,
-      coordinates: {
-        x: 0,
-        y: 0,
-      },
-    };
+  const mappedOverflowTiles = (rowLength: number) => {
+    const words: JSX.Element[][] = [];
     const pathLength = props.gameState.path.length;
     const letterLength = props.gameState.pathLetters.length;
 
-    let tileType = TileType.Empty;
-    let className = "";
+    let currentWord: JSX.Element[] = [];
 
-    if (index === letterLength - 1 && pathLength === letterLength - 1) {
-      tileType = TileType.Head;
-    } else if (index === letterLength - 1 && pathLength < letterLength) {
-      tileType = TileType.HeadWest;
-    } else if (index === pathLength) {
-      tileType = TileType.HeadEast;
-    } else if (index < letterLength - 1 && index > pathLength) {
-      tileType = TileType.Horizontal;
-    } else {
-      className = "tile-path-empty";
+    props.gameState.pathLetters.map((t, index) => {
+      const mod = index % rowLength;
+      const isReversed = words.length % 2 === 1;
+      const isEndOfLine = mod === rowLength - 1;
+      const isStartOfLine = mod === 0;
+
+      const tile: Tile = {
+        guess: t,
+        value: t,
+        coordinates: {
+          x: 0,
+          y: 0,
+        },
+      };
+
+      let tileType = TileType.Empty;
+      let className = "";
+
+      if (index === letterLength - 1 && pathLength === letterLength - 1) {
+        tileType = TileType.Head;
+      } else if (index === letterLength - 1 && pathLength < letterLength) {
+        if (isStartOfLine) {
+          tileType = TileType.HeadNorth;
+        } else {
+          tileType = isReversed ? TileType.HeadEast : TileType.HeadWest;
+        }
+      } else if (index === pathLength) {
+        if (isEndOfLine) {
+          tileType = TileType.HeadSouth;
+        } else {
+          tileType = isReversed ? TileType.HeadWest : TileType.HeadEast;
+        }
+      } else if (index < letterLength - 1 && index > pathLength) {
+        if (isEndOfLine) {
+          tileType = isReversed
+            ? TileType.CornerSouthEast
+            : TileType.CornerSouthWest;
+        } else if (isStartOfLine) {
+          tileType = isReversed
+            ? TileType.CornerNorthWest
+            : TileType.CornerNorthEast;
+        } else {
+          tileType = TileType.Horizontal;
+        }
+      } else {
+        className = "tile-path-empty";
+      }
+
+      currentWord.push(
+        <div className={className} key={`path-letter-${index}`}>
+          <GridTile isReadOnly={true} tile={tile} tileType={tileType} />
+        </div>
+      );
+
+      if ((index + 1) % rowLength == 0) {
+        words.push(currentWord);
+        currentWord = [];
+      }
+    });
+
+    const numInvisibleTiles =
+      letterLength <= rowLength ? 0 : rowLength - currentWord.length;
+
+    for (let i = 0; i < numInvisibleTiles; i++) {
+      currentWord.push(<InvisibleTile key={`path-letter-invisible-${i}`} />);
     }
+    words.push(currentWord);
 
-    return (
-      <div className={className} key={t}>
-        <GridTile isReadOnly={true} tile={tile} tileType={tileType} />
-      </div>
-    );
-  });
+    return words.map((arr, index) => {
+      if (index % 2 === 1) {
+        arr.reverse();
+      }
+      return (
+        <div
+          key={`word-path-${index}`}
+          className="word-container word-container-path"
+        >
+          {arr}
+        </div>
+      );
+    });
+  };
 
   return (
     <div className="grid-container grid-container-small">
-      <div className="word-container word-container-path">{mappedTiles}</div>
+      <div className="path-large">{mappedOverflowTiles(40)}</div>
+      <div className="path-medium">{mappedOverflowTiles(15)}</div>
+      <div className="path-small">{mappedOverflowTiles(10)}</div>
     </div>
   );
 }
